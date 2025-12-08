8a8f4a29f398ef74d28ca5006b78bbefd1a2e2e4
Tim Judkins <tim@radiusnetworks.com>
2025-12-08 09:58:01 -0500

    Far Auto with obelisk april tag to sort artifacts before shooting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFarRedSort.java
@@ -0,0 +1,138 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.control.ArtifactSorter;
+import org.firstinspires.ftc.teamcode.control.DrivingController;
+import org.firstinspires.ftc.teamcode.control.LimelightController;
+import org.firstinspires.ftc.teamcode.control.MechanismController;
+
+/** @noinspection unused*/
+@Autonomous(name="AutoFarRedSort", group="Autonomous", preselectTeleOp = "RedTeleop")
+public class AutoFarRedSort extends LinearOpMode {
+    private final DrivingController driveController = new DrivingController();
+    private final MechanismController mechanismController = MechanismController.getInstance();
+    private final ElapsedTime runtime = new ElapsedTime();
+    private final LimelightController limelightController = new LimelightController();
+
+    enum StateMachine {
+        READ_LIMELIGHT,
+        WAIT_FOR_SORT,
+        START_SHOOTER,
+        SHOOT_1,
+        SHOOT_2,
+        SHOOT_3,
+        WAIT_FOR_SHOTS_1,
+        WAIT_TO_DRIVE_1,
+        DRIVE_TO_PARK,
+        AT_TARGET
+    }
+
+    @Override
+    public void runOpMode() {
+        driveController.init(hardwareMap, telemetry, true);
+        mechanismController.init(hardwareMap, telemetry, true);
+        limelightController.init(hardwareMap, telemetry);
+
+        StateMachine stateMachine;
+        stateMachine = StateMachine.READ_LIMELIGHT;
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        resetRuntime();
+        limelightController.switchPipeline(0);
+
+        while (opModeIsActive()) {
+            driveController.updateOdometry();
+            mechanismController.update();
+
+            switch (stateMachine) {
+                case READ_LIMELIGHT:
+                    if (runtime.seconds() > 1.0) {
+                        ArtifactSorter.Pattern pattern = limelightController.detectPattern();
+                        switch (pattern) {
+                            case PPG:
+                                mechanismController.rotateBelly();
+                                break;
+                            case PGP:
+                                mechanismController.rotateBelly();
+                                mechanismController.rotateBelly();
+                                break;
+                        }
+                        stateMachine = StateMachine.WAIT_FOR_SORT;
+                        runtime.reset();
+                    }
+                    break;
+                case WAIT_FOR_SORT:
+                    if (mechanismController.isBellyAtTarget()) {
+                        stateMachine = StateMachine.START_SHOOTER;
+                        runtime.reset();
+                    }
+                    break;
+                case START_SHOOTER:
+                    //the first step in the autonomous
+                    stateMachine = StateMachine.SHOOT_1;
+                    mechanismController.setShooterVelocity(1900);
+                    mechanismController.setHoodUp();
+                    runtime.reset();
+                    break;
+                case SHOOT_1:
+                    if (mechanismController.isShooterAtSpeed()) {
+                        mechanismController.rotateBelly();
+                        stateMachine = StateMachine.SHOOT_2;
+                        runtime.reset();
+                    }
+                    break;
+                case SHOOT_2:
+                    if (runtime.seconds() >= 0.5) {
+                        mechanismController.rotateBelly();
+                        stateMachine = StateMachine.SHOOT_3;
+                        runtime.reset();
+                    }
+                    break;
+                case SHOOT_3:
+                    if (runtime.seconds() >= 0.5) {
+                        mechanismController.rotateBelly();
+                        stateMachine = StateMachine.WAIT_FOR_SHOTS_1;
+                        runtime.reset();
+                    }
+                    break;
+                case WAIT_FOR_SHOTS_1:
+                    if (mechanismController.isBellyAtTarget()) {
+                        stateMachine = StateMachine.WAIT_TO_DRIVE_1;
+                        runtime.reset();
+                    }
+                    break;
+                case WAIT_TO_DRIVE_1:
+                    if (runtime.seconds() >= 2.0) {
+                        mechanismController.stopShooter();
+                        runtime.reset();
+                        stateMachine = StateMachine.DRIVE_TO_PARK;
+                    }
+                    break;
+                case DRIVE_TO_PARK:
+                    // wait a half second before moving to let shooting finish
+                    driveController.updateDriveCommands(0.5, 0.0, 0.0, true);
+                    if (runtime.seconds() >= 1.0) {
+                        driveController.updateDriveCommands(0.0, 0.0, 0.0, true);
+                        stateMachine = StateMachine.AT_TARGET;
+                    }
+                    break;
+
+            }
+
+            DrivingController.WheelPower wheelPower;
+            if (stateMachine == StateMachine.AT_TARGET){
+                driveController.updateDriveCommands(0.0, 0.0, 0.0, true);
+            }
+
+            telemetry.addData("current state:",stateMachine);
+            mechanismController.updateTelemetry();
+
+            telemetry.update();
+
+        }
+    }
+}
